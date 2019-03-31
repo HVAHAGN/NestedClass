8a969e3b96934b6a74d36055afd39289355156ca
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 13:52:51 +0400

    Inheritance from BaseClass

---
+++ Nested_04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_04
+{
+    // Inheritance from BaseClass does not apply.
+    public class MyClass
+    {
+        public class Nested : BaseClass
+        {
+            public void MethodFromNested()
+            {
+                Console.WriteLine("Method from nested class");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Nested instance = new MyClass.Nested();
+            instance.MethodFromBase();
+            instance.MethodFromNested();
+
+
+            Console.ReadLine();
+        }
+    }
+}
