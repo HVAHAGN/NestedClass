e557427c896611374813884a32228013d697c6e9
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 13:37:52 +0400

    Nested class/private static field

---
+++ Nested_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_02
+{
+   class MyClass
+    {
+        private static int field = 0;
+        public class Nested
+        {
+            public void Method(int a)
+            {
+                field = a;
+                Console.WriteLine(field);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Nested instance = new MyClass.Nested();
+            instance.Method(2);
+
+            Console.ReadLine();
+        }
+    }
+}
