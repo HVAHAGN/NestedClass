b291d770aab79b65e548fff40fa4a9afee3faf4d
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 14:02:01 +0400

    External class inheritance from BaseClass does not apply to Nested

---
+++ Nested_05/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_05
+{
+    public class BaseClass
+    {
+        public void MethodFromBase()
+        {
+            Console.WriteLine("Method from base class");
+        }
+
+    }
+}
---
+++ Nested_05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_05
+{
+    // External class inheritance from BaseClass does not apply to Nested.
+    public class MyClass : BaseClass
+    {
+        public class Nested
+        {
+            public void MethodFromNested()
+            {
+                Console.WriteLine("Method from nested class");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance1 = new MyClass();
+            instance1.MethodFromBase();
+
+
+            MyClass.Nested instance2 = new MyClass.Nested();
+
+            instance2.MethodFromNested();
+
+
+            Console.ReadLine();
+        }
+    }
+}
