1c2774ab14cf62959b2882d7e906b26f4c93b3c0
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 14:13:30 +0400

    Non-static classes can contain static Nested classes.

---
+++ Nested_07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_07
+{
+    /// <summary>
+    ///Non-static classes can contain static Nested classes.
+    /// </summary>
+    /// 
+    public class MyClass
+    {
+        public static class Nested
+        {
+            public static void StaticMethodFromNested()
+            {
+                Console.WriteLine("Static method from nested");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Nested.StaticMethodFromNested();
+
+            
+            Console.ReadLine();
+        }
+    }
+}
