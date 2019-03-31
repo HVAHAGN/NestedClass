1c86df2d2fb61259e6f1a87ab395aa9c37e0c32a
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 15:05:47 +0400

    Delegation technique

---
+++ Delegation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegation
+{
+    // Delegation technique.
+    class A
+    {
+        public string DoSomething(string s)
+        {
+            //Console.WriteLine("Action");
+            return s + " |||";
+        }
+    }
+    class B
+    {
+        public string DoSomething(string s)
+        {
+            A a = new A();
+           return  a.DoSomething(s);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            string myShoes = b.DoSomething("Shoes to repair");
+            Console.WriteLine(myShoes);
+            Console.ReadLine();
+        }
+    }
+}
