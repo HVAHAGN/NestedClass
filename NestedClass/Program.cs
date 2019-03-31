dfb872a34c209ae5dcd43651edd340472db99f10
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 13:31:54 +0400

    Nested class example

--- NestedClass/Program.cs
+++ NestedClass/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace NestedClass
 {
+    class MyClass
+    {
+        public class NestedClass
+        {
+            public void Method()
+            {
+                Console.WriteLine("Method from nested class");
+            }
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            MyClass.NestedClass instance = new MyClass.NestedClass();
+            instance.Method();
+
+            Console.ReadLine();
         }
     }
 }
